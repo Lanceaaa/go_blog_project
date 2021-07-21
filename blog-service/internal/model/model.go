9eb096fc1ca7ca031f54ae32a195a6b2a61e7258
Lance <ljl@yuwulife.com>
2021-07-21 20:58:50 +0800

    完成标签model方法，处理model的回调(CreatedOn、ModifiedOn、IsDel)

--- blog-service/internal/model/model.go
+++ blog-service/internal/model/model.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-programming-tour-book/blog-service/global"
 	"github.com/go-programming-tour-book/blog-service/pkg/setting"
@@ -15,7 +16,7 @@ type Model struct {
 	ModifiedBy string `json:"modified_by"`
 	CreatedOn  uint32 `json:"created_on"`
 	ModifiedOn uint32 `json:"modified_on"`
-	deletedOn  uint32 `json:"deleted_on"`
+	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
 }
 
@@ -32,8 +33,81 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 		db.LogMode(true)
 	}
 	db.SingularTable(true)
+	// 注册回调行为
+	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
+
 	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
 	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
 
 	return db, nil
+}
+
+// 新增行为的回调
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		if createTimeFiled, ok := scope.FieldByName("CreatedOn"); ok {
+			if createTimeFiled.IsBlank {
+				_ = createTimeFiled.Set(nowTime)
+			}
+		}
+		if modifyTimeField, ok := scope.FieldByName("ModifyOn"); ok {
+			if modifyTimeField.IsBlank {
+				_ = modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+
+// 更新行为的回调
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+	// 调用scope.Get("gorm:update_column")来获取当前设置的标识gorm:update_column字段属性
+	if _, ok := scope.Get("gorm:update_column"); !ok {
+		// 否则更新ModifiedOn
+		_ = scope.SetColumn("ModifiedOn", time.Now().Unix())
+	}
+}
+
+// 删除行为的回调
+func deleteCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		var extarOption string
+		// 调用scope.Get("gorm:delete_column")来获取当前设置的标识gorm:delete_column字段属性
+		if str, ok := scope.Get("gorm:delete_column"); ok {
+			extarOption = fmt.Sprint(str)
+		}
+
+		// 判断是否存在DeletedOn和IsDel字段。若存在，则执行UPDATE进行软删除，否则执行DELETE操作进行硬删除
+		deletedOnField, hasDeletedOnField := scope.FieldByName("DeletedOn")
+		isDelField, hasIsDelField := scope.FieldByName("IsDel")
+		if !scope.Search.Unscoped && hasDeletedOnField && hasIsDelField {
+			now := time.Now().Unix()
+			scope.Raw(fmt.Sprintf(
+				"UPDATE %v SET %v=%v, %v=%v%v%v",
+				scope.QuotedTableName(),
+				scope.Quote(deletedOnField.DBName),
+				scope.AddToVars(now),
+				scope.Quote(isDelField.DBName),
+				scope.AddToVars(1),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extarOption),
+			)).Exec()
+		} else {
+			scope.Raw(fmt.Sprintf(
+				"DELETE FROM %v%v%v",
+				scope.QuotedTableName(),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extarOption),
+			)).Exec()
+		}
+	}
+}
+
+func addExtraSpaceIfExist(str string) string {
+	if str != "" {
+		return " " + str
+	}
+	return ""
 }
--- blog-service/internal/model/tag.go
+++ blog-service/internal/model/tag.go
@@ -2,10 +2,12 @@ package model
 
 import (
 	"github.com/go-programming-tour-book/blog-service/pkg/app"
+	"github.com/jinzhu/gorm"
 )
 
 type Tag struct {
 	*Model
+	ID    uint32 `json:"id"`
 	Name  string `json:"name"`
 	State uint8  `json:"state"`
 }
@@ -17,4 +19,53 @@ func (t Tag) TableName() string {
 type TagSwagger struct {
 	List  []*Tag
 	Pager *app.Pager
+}
+
+// 获取标签总数
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// 获取标签数据列表
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+// 创建标签
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+// 修改标签
+func (t Tag) Update(db *gorm.DB) error {
+	db = db.Model(&Tag{}).Where("id = ? AND is_del = ?", t.ID, 0)
+	return db.Update(t).Error
+}
+
+// 删除标签
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", t.ID, 0).Delete(&t).Error
 }
