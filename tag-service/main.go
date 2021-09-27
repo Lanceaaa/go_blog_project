32b8f0fad22732be8dd5784753b90efa59f0b16b
Lance <ljl@yuwulife.com>
2021-09-27 16:12:37 +0800

    通过使用第三方开源库 cmux 实现多协议支持（grpc、http）

--- tag-service/main.go
+++ tag-service/main.go
@@ -8,65 +8,65 @@ import (
 
 	pb "github.com/go-programming-tour-book/tag-service/proto"
 	"github.com/go-programming-tour-book/tag-service/server"
+	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-// var port string
-var grpcPort string
-var httpPort string
+var port string
 
 func init() {
-	flag.StringVar(&grpcPort, "grpc_port", "8001", "gRPC 启动端口号")
-	flag.StringVar(&httpPort, "http_port", "9001", "HTTP 启动端口号")
+	flag.StringVar(&port, "port", "8001", "启动端口号")
 	flag.Parse()
 }
 
+// 拆分 TCP 的逻辑
+func RunTCPServer(port string) (net.Listener, error) {
+	return net.Listen("tcp", ":"+port)
+}
+
 // 针对 HTTP 的 RunHttpServer 方法，其作用是初始化一个新的 HTTP 多路复用器，并新增了一个 /ping 路由及其 Handler，可用于做基本的心跳检测
-func RunHttpServer(port string) error {
+func RunHttpServer(port string) *http.Server {
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte(`pong`))
 	})
 
-	return http.ListenAndServe(":"+port, serveMux)
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: serveMux,
+	}
 }
 
 // 保持实现了 gRPC Server 的相关逻辑，仅是重新封装为 RunGrpcServer 方法
-func RunGrpcServer(port string) error {
+func RunGrpcServer() *grpc.Server {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
 
-	lis, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		log.Fatalf("net.Listen err: %v", err)
-	}
-
-	return s.Serve(lis)
+	return s
 }
 
 func main() {
-	// 监听 HTTP EndPoint 和 gRPC EndPoint 是一个阻塞的行为
-	errs := make(chan error)
-	// RunHttpServer 或 RunGrpcServer 方法启动或运行出现了问题，会将 err 写入 chan 中
-	go func() {
-		err := RunHttpServer(httpPort)
-		if err != nil {
-			errs <- err
-		}
-	}()
+	// 初始化 TCP Listener
+	l, err := RunTCPServer(port)
+	if err != nil {
+		log.Fatalf("Run TCP Server err: %v", err)
+	}
+	m := cmux.New(l)
+	// cmux 也是基于 application/grpc 标识去进行分流
+	grpcL := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldPrefixSendSettings("content-type", "application/grpc"))
+	httpL := m.Match(cmux.HTTP1Fast())
 
-	go func() {
-		err := RunGrpcServer(grpcPort)
-		if err != nil {
-			errs <- err
-		}
-	}()
+	grpcS := RunGrpcServer()
+	httpS := RunHttpServer(port)
+	// 使用 cmux 监听 gRPC 和 HTTP 服务
+	go grpcS.Serve(grpcL)
+	go httpS.Serve(httpL)
 
-	// 因此我们只需要利用 select 对其进行检测即可
-	select {
-	case err := <-errs:
-		log.Fatalf("Run Server err: %v", err)
+	// 开启服务
+	err = m.Serve()
+	if err != nil {
+		log.Fatalf("Run Serve err: %v", err)
 	}
 }
